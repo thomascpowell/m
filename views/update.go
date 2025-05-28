1dc8c599fe5e79ab479d4107010e5e7eb7eb8664
Thomas <thomascoxpowell@gmail.com>
2025-05-28 01:15:12 -0400

    moved update loop to seperate file

---
+++ views/update.go
@@ -0,0 +1,172 @@
+package views
+
+import (
+	"m/scripts"
+	"m/utils"
+	"time"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+/**
+* Contains the update loop and associated functions.
+*/
+
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case scripts.LibraryMsg:
+		return m.handleLibraryMsg(msg)
+	case scripts.StateMsg:
+		return m.handleStateMsg(msg)
+	case tickMsg:
+		return m.handleTickMsg(msg)
+	case scripts.ListMsg:
+		return m.handleListMsg(msg)
+	case tea.KeyMsg:
+		return m.handleKeyMsg(msg)
+	case tea.WindowSizeMsg:
+		if containsUIList(m.CurrentView) {
+			m.UIList.SetSize(msg.Width, msg.Height)
+		}
+	}
+	if containsUIList(m.CurrentView) {
+		var cmd tea.Cmd
+		m.UIList, cmd = m.UIList.Update(msg)
+		return m, cmd
+	}
+	return m, nil
+}
+
+func TickCmd() tea.Cmd {
+	return tea.Tick(time.Second*3, func(t time.Time) tea.Msg {
+		return tickMsg{}
+	})
+}
+type tickMsg struct{}
+
+func (m Model) handleLibraryMsg(msg scripts.LibraryMsg) (tea.Model, tea.Cmd) {
+	m.Library = utils.Library {
+		Songs: msg.Songs,
+		Albums: msg.Albums,
+		Playlists: msg.Playlists,
+	}
+	return m, nil
+}
+
+func (m Model) handleStateMsg(msg scripts.StateMsg) (tea.Model, tea.Cmd) {
+	m.CurrentSong = msg.CurrentSong
+	m.IsPlaying = msg.IsPlaying
+	return m, nil
+}
+
+func (m Model) handleTickMsg(msg tickMsg) (tea.Model, tea.Cmd) {
+	return m, tea.Batch(TickCmd(), scripts.RefreshStateCmd())
+}
+
+func (m Model) handleListMsg(msg scripts.ListMsg) (tea.Model, tea.Cmd) {
+	m.CurrentList = utils.List(msg)
+	m.UIList = NewDetailList(m.CurrentList.Songs, m.CurrentList.Name, m.CurrentList.Owner)
+	m.CurrentView = SourceDetailView
+	return m, nil
+}
+
+func (m Model) handleKeyMsg(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
+	if m.UIList.FilterState() == list.Filtering {
+		var cmd tea.Cmd
+		m.UIList, cmd = m.UIList.Update(msg)
+		return m, cmd
+	}
+	switch msg.String() {
+
+	// generic keybinds
+	case "ctrl+c", "q":
+		return m, tea.Quit
+	case " ":
+		m.IsPlaying = !m.IsPlaying 
+		return m, scripts.RunAsCmd("toggle", scripts.TogglePlayPause)
+	case "a":
+		m.UIList = NewSourceList(m.Library.Albums, "albums")
+		m.CurrentView = AlbumsView
+		return m, nil
+	case "p":
+		m.UIList = NewSourceList(m.Library.Playlists, "playlists")
+		m.CurrentView = PlaylistsView
+		return m, nil
+
+
+	// temporary
+	// will soon move this to a dedicated item in detail view
+	case "s":
+		selected := m.UIList.SelectedItem()
+		item, ok := selected.(utils.ListItem)
+		if !ok {
+			utils.Log("failed to start")
+			break
+		}
+		switch m.CurrentView {
+		case AlbumsView:	
+			s := utils.Source {
+				Title: item.Name,
+				Artist: item.Desc,
+			}
+			err := scripts.PlaySongList(scripts.GetSongsFromSource(utils.Album, s, m.Library))
+			if err != nil {
+				utils.Log("AlbumS: " + err.Error())
+			}
+			return m, nil
+		case PlaylistsView:
+			err := scripts.PlayPlaylist(item.Name)
+			if err != nil {
+				utils.Log("PlaylistS: " + err.Error())
+			}
+			return m, nil
+		}
+	// "back" or "base"
+	case "b":
+		m.CurrentView = BaseView
+		return m, nil
+	// select item
+	case "enter":
+		return m.handleSelect()
+	}
+	// pass other keypresses to UIList
+	var cmd tea.Cmd
+	m.UIList, cmd = m.UIList.Update(msg)
+	return m, cmd
+}
+
+func (m Model) handleSelect() (tea.Model, tea.Cmd) {
+	selected := m.UIList.SelectedItem()
+	item, ok := selected.(utils.ListItem)
+	if !ok {
+		return m, nil
+	}
+	s := utils.Source {
+		Title: item.Name,
+		Artist: item.Desc,
+	}
+	switch m.CurrentView {
+	case SourceDetailView:
+		return m, scripts.RunAsCmd("play track", func() error {
+			return scripts.SelectTrack(item.Id)
+		})
+	case AlbumsView:
+		return m, scripts.UpdateListCmd(utils.Album, s, m.Library)
+	case PlaylistsView:
+		return m, scripts.UpdateListCmd(utils.Playlist, s, m.Library)
+	default:
+		break
+	}
+	return m, nil
+}
+
+
+
+func containsUIList(view View) bool {
+	return view == AlbumsView || 
+	view == PlaylistsView || 
+	view == SourceDetailView 
+}
+
+
